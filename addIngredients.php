1e8742b35d368190071e99c0935f83a8fcb67f00
Kevin Mangus <ktmangus@baton-rouge.cs.colostate.edu>
2017-04-08 20:50:41 -0600

    added addIngredients

--- addIngredients.php
+++ addIngredients.php
@@ -5,11 +5,46 @@ $helpPage = FALSE;
 require_once "inc/page_setup.php";
 if (!$dbh = setupDB()) {die;}
 include 'inc/header.php';
+
+$ingName = $fileName = $price = $description = $attributes ='';
+
+if(isset($_POST["submitfrm"])){
+    $ingName = $_POST['ingName']; 
+    $fileName = $_POST['fileName'];
+    $price = $_POST['price']; 
+    $description = $_POST['description'];
+    $attributes = $_POST['attributes'];
+
+    addIngredient($ingName, $fileName, $price);
+    
+}
+
 ?>
-<form name='input' action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'> 
 
-<input type='text' value='' id='increase' name='username' placeholder='Enter Username'/>
+<div id="addIngForm">
+    <h2 align="center">Add Ingredients</h2>
+    <form name='input' action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post' id="formInput" align="center"> 
 
-</form>
+        <input id="formBox" type='text' value='<?php echo $ingName; ?>' id='ingName' name='ingName' placeholder='Enter Ingredient Name'/>
+        <br/>
+
+        <input id="formBox" type='text' value='<?php echo $fileName; ?>' id='fileName' name='fileName' placeholder='Enter File Name'/>
+        <br/>
+
+        <input id="formBox" type='text' value='<?php echo $price; ?>' id='price' name='price' placeholder='Enter Price'/>
+        <br/>
+
+        <textarea value='<?php echo $description; ?>' style="margin-top: 25px; margin-left: 50px;" rows="4" cols="50" id="formInput" placeholder='Enter description'></textarea>
+        <br/>
+        
+        <textarea  value=''<?php echo $attributes; ?>' style="margin-top: 25px; margin-left: 50px;" rows="4" cols="50" id="formInput" placeholder='Enter Attributes'></textarea>
+        <br/>
+
+        <input type='submit' value='submitfrm' name='submitfrm' class="submitfrm" />
+
+    </form>
+    
+
+</div>
 
 <?php include 'inc/footer.php'; 
